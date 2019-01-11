85ba3f8ebedaf94348a3666f9f21941c31d3273d
guiyuan <1176916155@qq.com>
2019-01-11 08:03:31 +0800

    需要修稿form的前端渲染

--- apps/printers/urls.py
+++ apps/printers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import updatepr
+from .views import updatepr,PrintCreate
 
 urlpatterns = [
     # 打印机更新
     path('updatepr/<int:pk>/',  updatepr.as_view(), name='update'),
     # path('prdelete/<int:id>/', tododelete, name='delete'),
+    path('create/',  PrintCreate.as_view(), name='create'),
 ]
--- apps/printers/views.py
+++ apps/printers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.views.generic.base import View
 from django.views.generic import DetailView, ListView, UpdateView
+from django.views.generic.edit import CreateView
 from django.http import Http404
 # Create your views here.
 
@@ -13,7 +14,7 @@ class updatepr(View):
         return render(request, 'updatepr.html', {'printers':printers})
 
 
-
+# 更新打印机信息
 class updatepr(UpdateView):
     model = Prints
     template_name = 'form.html'
@@ -25,3 +26,14 @@ class updatepr(UpdateView):
             raise Http404()
         return obj
 
+
+# 创建打印机信息
+class PrintCreate(CreateView):
+    model = Prints
+    template_name = 'form.html'
+    form_class = PrintForm
+
+    # Associate form.instance.user with self.request.user
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
